b3162f547c10b57c8c75dbaf940fd04e3fea3ea5
Terry Duong <tduong10101@gmail.com>
2023-09-17 22:58:42 +1000

    updated encrypt method to use scrypt

--- website/auth.py
+++ website/auth.py
@@ -50,7 +50,7 @@ def sign_up():
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
-            new_user=User(email=email,first_name=first_name,password=generate_password_hash(password1, method='sha256'))
+            new_user=User(email=email,first_name=first_name,password=generate_password_hash(password1, method='scrypt'))
             db.session.add(new_user)
             db.session.commit()
             flash('Account created!', category='success')
