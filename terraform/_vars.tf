bbf1d5f712f3bd72aa6ec7e5bf0a8edfdfe10ed5
Terry Duong <tduong10101@gmail.com>
2023-10-03 22:14:17 +1100

    added rds resources

---
+++ terraform/_vars.tf
@@ -0,0 +1,4 @@
+variable "db_pass" {
+    sensitive = true
+    type = string
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,10 @@
-resource "aws_kms_key" "tnote-mysql-secret"{
-    description = "tnote mysql secret"
-}
-
 resource "aws_db_instance" "tnote-db" {
     allocated_storage = 10
     db_name = "database"
     engine = "mysql"
     instance_class = "db.t3.micro"
-    manage_master_user_password = true
-    master_user_secret_kms_key_id = aws_kms_key.tnote-mysql-secret.key_id
     username = "root"
+    password = var.db_pass
 }
 
 output "rds_endpoint" {
