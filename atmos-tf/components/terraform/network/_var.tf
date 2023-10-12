abba346c440bb5e03f379c5a0bc6a1ccbcf91fa4
Terry Duong <tduong10101@gmail.com>
2023-10-12 14:23:57 +1100

    restructure atmos

--- atmos-tf/components/terraform/network/_var.tf
+++ atmos-tf/components/terraform/network/_var.tf
@@ -2,3 +2,11 @@ variable "ipv4_cidr" {
     type=string
     description = "ipv4 cidr block"
 }
+variable "namespace" {
+    type=string
+    description = "namespace"
+}
+variable "stage" {
+    type=string
+    description = "stage"
+}
