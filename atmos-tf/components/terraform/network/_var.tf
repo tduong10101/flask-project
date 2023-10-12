cef10ca8b79666f21896bce4bd07bb09dc9912d0
Terry Duong <tduong10101@gmail.com>
2023-10-12 23:59:35 +1100

    updated network tf module

--- atmos-tf/components/terraform/network/_var.tf
+++ atmos-tf/components/terraform/network/_var.tf
@@ -10,3 +10,18 @@ variable "stage" {
     type=string
     description = "stage"
 }
+variable "sn1_ipv4_cidr" {
+    type=string
+    description = "ipv4 cidr block"
+    default = "192.168.1.0/24"
+}
+variable "sn2_ipv4_cidr" {
+    type=string
+    description = "ipv4 cidr block"
+    default = "192.168.2.0/24"
+}
+variable "sn3_ipv4_cidr" {
+    type=string
+    description = "ipv4 cidr block"
+    default = "192.168.3.0/24"
+}
---
+++ atmos-tf/components/terraform/network/main.tf
@@ -0,0 +1,61 @@
+resource "aws_vpc" "main" {
+    cidr_block = var.ipv4_cidr
+
+    tags = {
+        Name = var.namespace
+        Stage = var.stage
+    }
+}
+
+resource "aws_internet_gateway" "gateway" {
+    vpc_id = aws_vpc.main.id
+
+    tags = {
+        Name = var.namespace
+        Stage = var.stage
+    }
+}
+
+resource "aws_default_route_table" "route_table" {
+    default_route_table_id = aws_vpc.main.default_route_table_id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.gateway.id
+    }
+
+    tags = {
+        Name = var.namespace
+        Stage = var.stage
+    }
+}
+
+resource "aws_subnet" "sn1" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = var.sn1_ipv4_cidr
+
+    tags = {
+        Name = var.namespace
+        Stage = var.stage
+    }
+}
+
+resource "aws_subnet" "sn2" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = var.sn2_ipv4_cidr
+
+    tags = {
+        Name = var.namespace
+        Stage = var.stage
+    }
+}
+
+resource "aws_subnet" "sn3" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = var.sn3_ipv4_cidr
+
+    tags = {
+        Name = var.namespace
+        Stage = var.stage
+    }
+}
