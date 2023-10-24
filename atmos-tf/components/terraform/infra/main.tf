33b580428b28336823d9e4c96b65d850e7ce3577
Terry Duong <tduong10101@gmail.com>
2023-10-24 21:32:52 +1100

    added ecs task def

--- atmos-tf/components/terraform/infra/main.tf
+++ atmos-tf/components/terraform/infra/main.tf
@@ -1,12 +1,37 @@
-resource "aws_db_instance" "tnote-db" {
+resource "aws_db_instance" "tnote_db" {
     allocated_storage = 10
-    db_name = "tnote-db"
+    db_name = "tnotedb"
     engine = "mysql"
     instance_class = "db.t3.micro"
     username = "root"
     password = var.db_pass
+    port=3306
+    publicly_accessible = true
+    skip_final_snapshot = true
 }
-
-output "rds_endpoint" {
-    value = aws_db_instance.tnote-db.endpoint
+resource "aws_ecs_task_definition" "tnote" {
+    family = "tnote"
+    container_definitions =  jsonencode([
+        {
+            name = "tnote"
+            image = "069363837566.dkr.ecr.ap-southeast-2.amazonaws.com/my-ecr-repo"
+            memory = 2000
+            environment = [
+                {
+                    name = "tnote"
+                    SQL_USERNAME = aws_db_instance.tnote_db.username
+                    SQL_PASSWORD = var.db_pass
+                    SQL_HOST = aws_db_instance.tnote_db.address
+                    SQL_PORT = aws_db_instance.tnote_db.port
+                    DB_NAME = aws_db_instance.tnote_db.db_name
+                }
+            ]
+            portMappings = [
+                {
+                    containerPort=8000
+                    hostPort=8000
+                }
+            ]
+        }    
+    ])
 }
