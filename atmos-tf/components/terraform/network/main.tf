1f18295178d32c28ca455bf27964dd1aef699755
Terry Duong <tduong10101@gmail.com>
2023-10-24 22:47:17 +1100

    updated ingress and egress format

--- atmos-tf/components/terraform/network/main.tf
+++ atmos-tf/components/terraform/network/main.tf
@@ -68,45 +68,38 @@ resource "aws_security_group" "tnote_ecs_sg" {
   name   = "tnote_ecs_sg"
   vpc_id = aws_vpc.main.id
 
-  ingress = [
-    {
-      from_port        = 8000
-      to_port          = 8000
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-    },
-  ]
-  egress = [
-    {
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-    },
-  ]
+  ingress {
+    from_port        = 8000
+    to_port          = 8000
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 }
 resource "aws_security_group" "tnote_lb_sg" {
   name   = "tnote_lb_sg"
   vpc_id = aws_vpc.main.id
 
-  ingress = [
-    {
-      from_port        = 443
-      to_port          = 443
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-    },
-  ]
-  egress = [
-    {
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-    },
-  ]
+  ingress {
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 }
