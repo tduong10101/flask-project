4cc81db0119fb06b1cc5d20a4d55146ee48fcfad
Terry Duong <tduong10101@gmail.com>
2023-10-31 23:38:17 +1100

    updated sg with ssh permission

--- atmos-tf/components/terraform/network/main.tf
+++ atmos-tf/components/terraform/network/main.tf
@@ -76,6 +76,13 @@ resource "aws_security_group" "tnote_ecs_sg" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
@@ -88,6 +95,13 @@ resource "aws_security_group" "tnote_lb_sg" {
   name   = "${var.namespace}_lb_sg"
   vpc_id = aws_vpc.main.id
 
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
   ingress {
     from_port        = 443
     to_port          = 443
