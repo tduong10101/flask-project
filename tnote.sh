565b220a0b30981eb37a8f0982a2fa25f004e8b7
Terry Duong <tduong10101@gmail.com>
2023-11-05 09:12:52 +1100

    updated sh

--- tnote.sh
+++ tnote.sh
@@ -1,2 +1,2 @@
 cd /home/python/tnote
-/home/python/.local/bin/gunicorn -w 4 -b 0.0.0.0:8000 "website:create_app()"
+nohup /home/python/.local/bin/gunicorn -w 4 -b 0.0.0.0:8000 "website:create_app()" &
