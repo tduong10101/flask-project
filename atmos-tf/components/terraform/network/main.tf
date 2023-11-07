f63f04add73f3ed26d56c5efb2546b63ae28b214
Terry Duong <tduong10101@gmail.com>
2023-11-07 22:57:41 +1100

    added db sg

--- atmos-tf/components/terraform/network/main.tf
+++ atmos-tf/components/terraform/network/main.tf
@@ -117,3 +117,23 @@ resource "aws_security_group" "tnote_lb_sg" {
     ipv6_cidr_blocks = ["::/0"]
   }
 }
+
+resource "aws_security_group" "tnote_db_sg" {
+  name   = "${var.namespace}_db_sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = ["${aws_security_group.tnote_ecs_sg}"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
